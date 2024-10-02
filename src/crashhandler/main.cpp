82b86101812431f82df7b179586d1b7d1f549523
surveykshak <49113452+surveykshak@users.noreply.github.com>
2024-10-02 18:43:07 +0530

    Update main.cpp

--- src/crashhandler/main.cpp
+++ src/crashhandler/main.cpp
@@ -39,8 +39,8 @@ int main( int argc, char *argv[] )
 
   QApplication app( argc, argv );
   QApplication::setQuitOnLastWindowClosed( true );
-  QCoreApplication::setOrganizationName( "QGIS" );
-  QCoreApplication::setApplicationName( "QGIS3" );
+  QCoreApplication::setOrganizationName( "QUICKGIS38" );
+  QCoreApplication::setApplicationName( "QUICKGIS3_38" );
 
   QString extraInfoFile = QString( argv[1] );
   std::cout << "Extra Info File: " << extraInfoFile.toUtf8().constData() << std::endl;
